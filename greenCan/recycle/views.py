53cb36b1624f7b8c1f7bc1edd8ba8ec2a32e8bd5
bojianwangny <bw2197@nyu.edu>
2022-03-01 02:57:14 -0500

    sending zip code to backend

--- greenCan/recycle/views.py
+++ greenCan/recycle/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 def index(request):
     template = 'index.html'
     return render(request, template, context={})
+
+
+def search_locations_by_zipcode(request):
+    zipcode = request.GET.get('zipcode')
+    print(zipcode)
