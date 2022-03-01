ca1b7ba173c1a192f2077b1bb1d54078cc36f7ee
pb2640 <pb2640@nyu.edu>
2022-03-01 17:16:13 -0500

    added sorting distance on zip code and long-lat input

--- greenCan/recycle/views.py
+++ greenCan/recycle/views.py
@@ -1,5 +1,50 @@
+
 from django.shortcuts import render
+import requests
+import pandas as pd
+from geopy import distance
+from . import models
+# from models import ZipCode
 
 def index(request):
     template = 'index.html'
     return render(request, template, context={})
+
+
+#user location is the location of the user i.e in form of latitude,longitude and if zipcode 
+# was entered by the user, then input shall be the centroid of the area
+# response for all_locations should be something like : response = requests.get("https://data.cityofnewyork.us/resource/sxx4-xhzg.json")
+def min_dist_lat_long(user_location,all_locations):
+    #check if user_location is a zip code or a tuple of latitude/longitude
+    #if zipcode
+    try:
+        if(type(user_location)==int):
+            zip = int(user_location)
+            for it in models.ZipCode.objects.filter(zip_code=zip).values():
+                my_location = (it["centroid_latitude"],it["centroid_longitude"])
+        elif(type(user_location)==tuple):
+            latitude,longitude = user_location[0],user_location[1]
+            my_location = (latitude,longitude)
+    except:
+        return {"Location could not be found"}
+        
+
+    #calculate distance for all locations
+    response = all_locations
+    df = pd.DataFrame.from_dict(response.json(), orient='columns')
+    df["distance"] = 0
+     
+    for i in range(len(df)):
+        try:
+            site_location = (df["latitude"].iloc[i],df["longitude"].iloc[i])
+            df["distance"].iloc[i] = distance.distance(my_location,site_location).km
+        except:
+            df["distance"].iloc[i] = float("inf")
+            #dont display inf in sorted list 
+    df = df.sort_values(by=['distance'])
+    return df.to_dict()
+
+
+
+
+    
