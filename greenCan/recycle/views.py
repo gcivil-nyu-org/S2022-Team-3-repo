291a3ad5cbd98840d70bf0d777599c963d923870
bojianwangny <bw2197@nyu.edu>
2022-03-01 18:46:49 -0500

    rebuilding json data

--- greenCan/recycle/views.py
+++ greenCan/recycle/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
+
+from .models import *
 
 def index(request):
     template = 'index.html'
@@ -7,4 +11,35 @@ def index(request):
 
 def search_locations_by_zipcode(request):
     zipcode = request.GET.get('zipcode')
-    print(zipcode)
+    err_flag = False
+    err_msg = ''
+    zip_locations = ZipCode.objects.filter(zip_code=zipcode)
+    if zip_locations == 0:
+        err_flag = True
+        err_msg = "Please enter a valid NYC zip code"
+        return JsonResponse({'err_flag': err_flag, 'err_msg': err_msg})
+    else:
+        centroid = {'latitude': zip_locations[0].centroid_latitude, 'longitude': zip_locations[0].centroid_longitude}
+        # print(centroid)
+        locations = DropOffLocation.objects.all()
+        sites = []
+        for i in range(locations.count()):
+            name = locations[i].name
+            zip_code = locations[i].zip_code.zip_code
+            state_id = locations[i].zip_code.state_id
+            latitude = locations[i].latitude
+            longitude = locations[i].longitude
+            items_accepted = locations[i].items_accepted
+            category = locations[i].type
+            email = locations[i].public_email
+            phone_number = locations[i].phone_number
+            street_address = locations[i].street_address
+            site = {'name': name, 'zip_code': zip_code, 'state_id': state_id, 'latitude': latitude,
+                    'longitude': longitude, 'item_accepted': items_accepted, 'type': category,
+                    'email': email, 'phone_number': phone_number, 'street_address': street_address}
+            sites.append(site)
+
+        search_result = {"centroid": centroid, 'sites': sites}
+        json_data = json.dumps(search_result)
+        # print(json_data)
+        # print(type(json_data))
