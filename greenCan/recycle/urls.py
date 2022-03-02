39d9d723e6129536be2223cd65201d94a55f3240
bojianwangny <bw2197@nyu.edu>
2022-03-01 19:50:48 -0500

    add method for searching by user current location

--- greenCan/recycle/urls.py
+++ greenCan/recycle/urls.py
@@ -6,5 +6,6 @@ app_name = 'recycle'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('zipcode-search', views.search_locations_by_zipcode, name='search-location-zip')
+    path('zipcode-search', views.search_locations_by_zipcode, name='search-location-zip'),
+    path('currLoc-search', views.search_locations_by_current_location, name='search-location-currLoc'),
 ]
