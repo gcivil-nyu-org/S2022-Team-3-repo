bbb363dfced501d870b3b730fe26ee866ca54808
bojianwangny <bw2197@nyu.edu>
2022-03-02 01:23:33 -0500

    remove comment line

--- greenCan/recycle/views.py
+++ greenCan/recycle/views.py
@@ -93,6 +93,7 @@ def search_locations_by_current_location(request):
     centroid = {'latitude': user_lat, 'longitude': user_long}
     locations = DropOffLocation.objects.all()
     sites = []
+
     for i in range(locations.count()):
         name = locations[i].name
         zip_code = locations[i].zip_code.zip_code
