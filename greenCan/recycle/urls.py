7d39d822e8a35d2765bbc9dfce3a4dab8ee315a3
Kevin Patel <patelkvin04@gmail.com>
2022-03-04 02:13:42 -0500

    Optimized the Query for large dataset

--- greenCan/recycle/urls.py
+++ greenCan/recycle/urls.py
@@ -6,6 +6,5 @@ app_name = 'recycle'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('zipcode-search', views.search_locations_by_zipcode, name='search-location-zip'),
-    path('currLoc-search', views.search_locations_by_current_location, name='search-location-currLoc'),
+    path('fetch-drop-off-locations', views.search_dropoff_locations, name='fetch-drop-off-locations'),
 ]
--- greenCan/recycle/views.py
+++ greenCan/recycle/views.py
@@ -4,121 +4,142 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import *
-
+from django.core import serializers
 
 def index(request):
     template = 'index.html'
-    return render(request, template, context={})
+    types = DropOffLocation.objects.values_list('type',flat=True).distinct()
+    context = {
+        'types': types
+    }
+    return render(request, template, context=context)
 
 
 # user location is the location of the user i.e in form of latitude,longitude and if zipcode
 # was entered by the user, then input shall be the centroid of the area
 # response for all_locations should be something like :
 # response = requests.get("https://data.cityofnewyork.us/resource/sxx4-xhzg.json")
-def min_dist_lat_long(search_result):
-    search_result = search_result
-    # check if user_location is a zip code or a tuple of latitude/longitude
-    # if zipcode
+# def min_dist_lat_long(search_result):
+#     search_result = search_result
+#     # check if user_location is a zip code or a tuple of latitude/longitude
+#     # if zipcode
 
-    centroid_dict = search_result["centroid"]
-    site_dict = search_result["sites"]
+#     centroid_dict = search_result["centroid"]
+#     site_dict = search_result["sites"]
 
-    my_location = (centroid_dict["latitude"], centroid_dict["longitude"])
+#     my_location = (centroid_dict["latitude"], centroid_dict["longitude"])
     
-    # calculate distance for all locations
+#     # calculate distance for all locations
     
-    df = pd.DataFrame.from_dict(site_dict, orient='columns')
-    df["distance"] = 0
+#     df = pd.DataFrame.from_dict(site_dict, orient='columns')
+#     df["distance"] = 0
      
-    for i in range(len(df)):
-        try:
-            site_location = (df["latitude"].iloc[i],df["longitude"].iloc[i])
-            df["distance"].iloc[i] = distance.distance(my_location,site_location).km
-        except:
-            df["distance"].iloc[i] = float("inf")
-            # dont display inf in sorted list
-    df = df.sort_values(by=['distance'])
+#     for i in range(len(df)):
+#         try:
+#             site_location = (df["latitude"].iloc[i],df["longitude"].iloc[i])
+#             df["distance"].iloc[i] = distance.distance(my_location,site_location).km
+#         except:
+#             df["distance"].iloc[i] = float("inf")
+#     df = df.sort_values(by=['distance'])
+#     search_result = {"centroid": centroid_dict, 'sites': eval(df.to_json(orient='records'))}
+#     return search_result
 
-    # locations = df
-    # sites = []
-    # for i in range(len(locations)):
-    #     name = locations["name"][i]
-    #     zip_code = locations["zip_code"][i].
-    #     state_id = locations[i].zip_code.state_id
-    #     latitude = locations[i].latitude
-    #     longitude = locations[i].longitude
-    #     items_accepted = locations[i].items_accepted
-    #     category = locations[i].type
-    #     email = locations[i].public_email
-    #     phone_number = locations[i].phone_number
-    #     street_address = locations[i].street_address
-    #     site = {'name': name, 'zip_code': zip_code, 'state_id': state_id, 'latitude': latitude,
-    #             'longitude': longitude, 'item_accepted': items_accepted, 'type': category,
-    #             'email': email, 'phone_number': phone_number, 'street_address': street_address}
-    #     sites.append(site)
-    search_result = {"centroid": centroid_dict, 'sites': eval(df.to_json(orient='records'))}
-    return search_result
-
-@csrf_exempt
-def search_locations_by_zipcode(request):
-    zipcode = request.GET.get('zipcode')
-    zip_locations = ZipCode.objects.filter(zip_code=zipcode)
-    if zip_locations.count() == 0:
-        err_flag = True
-        err_msg = "Please enter a valid NYC zip code"
-        return JsonResponse({'err_flag': err_flag, 'err_msg': err_msg})
-    else:
-        centroid = {'latitude': zip_locations[0].centroid_latitude, 'longitude': zip_locations[0].centroid_longitude}
-        locations = DropOffLocation.objects.all()
-        sites = []
-        for i in range(locations.count()):
-            name = locations[i].name
-            zip_code = locations[i].zip_code.zip_code
-            state_id = locations[i].zip_code.state_id
-            latitude = locations[i].latitude
-            longitude = locations[i].longitude
-            items_accepted = locations[i].items_accepted if locations[i].items_accepted else ''
-            category = locations[i].type
-            email = locations[i].public_email if locations[i].public_email else ''
-            phone_number = locations[i].phone_number if locations[i].phone_number else ''
-            street_address = locations[i].street_address
-            site = {'name': name, 'zip_code': zip_code, 'state_id': state_id, 'latitude': latitude,
-                    'longitude': longitude, 'item_accepted': items_accepted, 'type': category,
-                    'email': email, 'phone_number': phone_number, 'street_address': street_address}
-            sites.append(site)
-
-        search_result = {"centroid": centroid, 'sites': sites}
-        sorted_json_data = min_dist_lat_long(search_result)
-        sorted_json_data['err_flag'] = False
-        return JsonResponse(sorted_json_data)
-
-
-@csrf_exempt
-def search_locations_by_current_location(request):
-    user_lat = request.GET.get('latitude')
-    user_long = request.GET.get('longitude')
-    centroid = {'latitude': user_lat, 'longitude': user_long}
-    locations = DropOffLocation.objects.all()
+def get_dropoff_locations(centroid):
+    logs_template = {
+        'fields': [':local'],
+    }
+    sites_template = {
+        'fields' : [':local',':related'],
+        'related':{
+            'logs': logs_template,
+        }
+    }
+    locations = DropOffLocation.objects.raw(f'''
+                SELECT F.ID AS ID,
+                F.NAME AS NAME, 
+                F.LATITUDE AS LATITUDE, 
+                F.LONGITUDE AS LONGITUDE, 
+                F.TYPE AS TYPE,
+                COALESCE(F.ITEMS_ACCEPTED,'') AS ITEMS_ACCEPTED, 
+                COALESCE(F.PUBLIC_EMAIL,'') AS EMAIL, 
+                COALESCE(F.PHONE_NUMBER,'') AS PUBLIC_PHONE,
+                F.ZIP_CODE_ID AS ZIP_CODE_ID FROM 
+                    (
+                        SELECT *,row_number() over (partition by TYPE order by D.DISTANCE desc) as TYPE_RANK FROM 
+                        (
+                        SELECT *,calculate_distance(40.627038489795, -74.02457508163266, R.LATITUDE, R.LONGITUDE, 'M') AS DISTANCE
+                            FROM RECYCLE_DROPOFFLOCATION AS R
+                            ORDER BY DISTANCE
+                        ) AS D
+                    ) AS F
+                WHERE F.TYPE_RANK<11''').prefetch_related('zip_code')
     sites = []
-    for i in range(locations.count()):
-        name = locations[i].name
-        zip_code = locations[i].zip_code.zip_code
-        state_id = locations[i].zip_code.state_id
-        latitude = locations[i].latitude
-        longitude = locations[i].longitude
-        items_accepted = locations[i].items_accepted if locations[i].items_accepted else ''
-        category = locations[i].type
-        email = locations[i].public_email if locations[i].public_email else ''
-        phone_number = locations[i].phone_number if locations[i].phone_number else ''
-        street_address = locations[i].street_address
+    for location in locations:
+        name = location.name
+        zip_code = location.zip_code.zip_code
+        state_id = location.zip_code.state_id
+        latitude = location.latitude
+        longitude = location.longitude
+        items_accepted = location.items_accepted if location.items_accepted else ''
+        category = location.type
+        email = location.public_email if location.public_email else ''
+        phone_number = location.phone_number if location.phone_number else ''
+        street_address = location.street_address
         site = {'name': name, 'zip_code': zip_code, 'state_id': state_id, 'latitude': latitude,
                 'longitude': longitude, 'item_accepted': items_accepted, 'type': category,
                 'email': email, 'phone_number': phone_number, 'street_address': street_address}
         sites.append(site)
+    # sites = serializers.serialize('json',locations,**sites_template)
+    # print(sites)
+    # print("=="*100)
+    # print(sites[0])
+    return sites
 
+@csrf_exempt
+def search_dropoff_locations(request):
+    if request.GET.get('type')=='zipcode':
+        zipcode = request.GET.get('zipcode')
+        zip_location = ZipCode.objects.filter(zip_code=zipcode).first()
+        if zip_location ==  None:
+            err_flag = True
+            err_msg = "Please enter a valid NYC zip code"
+            return JsonResponse({'err_flag': err_flag, 'err_msg': err_msg})
+        else:
+            centroid = {'latitude': zip_location.centroid_latitude, 'longitude': zip_location.centroid_longitude}
+    elif request.GET.get('type')=='live-location':
+        user_lat = request.GET.get('latitude')
+        user_long = request.GET.get('longitude')
+        centroid = {'latitude': user_lat, 'longitude': user_long}
+    else:
+        err_msg = "Invalid arguments provided"
+        return JsonResponse({'err_flag': err_flag, 'err_msg': err_msg})
+    sites = get_dropoff_locations(centroid)
     search_result = {"centroid": centroid, 'sites': sites}
-    sorted_json_data = min_dist_lat_long(search_result)
-    sorted_json_data['err_flag'] = False
-    return JsonResponse(sorted_json_data)
+    search_result['err_flag'] = False
+    return JsonResponse(search_result)
+    
+# @csrf_exempt
+# def search_locations_by_zipcode(request):
+#     zipcode = request.GET.get('zipcode')
+#     zip_location = ZipCode.objects.filter(zip_code=zipcode).first()
+#     if zip_location ==  None:
+#         err_flag = True
+#         err_msg = "Please enter a valid NYC zip code"
+#         return JsonResponse({'err_flag': err_flag, 'err_msg': err_msg})
+#     else:
+#         centroid = {'latitude': zip_location.centroid_latitude, 'longitude': zip_location.centroid_longitude}
+#         sites = get_dropoff_locations(centroid)
+#         search_result = {"centroid": centroid, 'sites': sites}
+#         search_result['err_flag'] = False
+#         return JsonResponse(search_result)
 
 
+# @csrf_exempt
+# def search_locations_by_current_location(request):
+#     user_lat = request.GET.get('latitude')
+#     user_long = request.GET.get('longitude')
+#     centroid = {'latitude': user_lat, 'longitude': user_long}
+#     sites = get_dropoff_locations(centroid)
+#     search_result = {"centroid": centroid, 'sites': sites}
+#     search_result['err_flag'] = False
+#     return JsonResponse(search_result)
