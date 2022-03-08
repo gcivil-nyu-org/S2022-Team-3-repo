c36d966759533085e2c40007c04255e824defb10
pb2640 <pb2640@nyu.edu>
2022-03-08 01:28:00 -0500

    added fetching details

--- greenCan/reuse/views.py
+++ greenCan/reuse/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 from .models import Post,Image
 from recycle.models import ZipCode
 
+def index(request):
+    template = 'reuse_index.html'
+    return render(request, template, context={})
+
 def donation_view(request):
     template = "donate-item-page.html"
     context = {"is_reuse": True}
@@ -18,8 +22,8 @@ def listingPage(request):
         # add the url
         Posts[index]["url"] = Images.filter(post_id = Posts[index]["id"])[0]["url"]
         temp = ZipCodes.filter(id = Posts[index]["zip_code_id"])[0]
-        Posts[index]["location"] = str(temp["borough"]+" "+temp["state"])
+        Posts[index]["location"] = str(temp["borough"]+", "+temp["state"]+", "+temp["zip_code"])
         
 
-    context = {"Posts":Posts}
+    context = {"Posts":Posts,"is_reuse": True}
     return render(request,template,context = context)
