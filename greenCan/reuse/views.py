e1eca9e71087e23286a2636a65d9b39b6bd03861
TheHarbinger99 <67739920+TheHarbinger99@users.noreply.github.com>
2022-03-08 21:17:18 -0500

    Update views.py

--- greenCan/reuse/views.py
+++ greenCan/reuse/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-
+from .forms import FormPost
 
 def donation_view(request):
     template = "donate-item-page.html"
     context = {"is_reuse": True}
     return render(request, template, context=context)
+
+def showform(request):
+    form= FormPost(request.POST or None)
+    if form.is_valid():
+        form.save()
+  
+    context= {'form': form }
+        
+    return render(request, 'adpostui.html', context)
+
