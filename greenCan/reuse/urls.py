18f2d9de8b798cd6be8341dee8b63538ff416ff3
Kevin Patel <patelkvin04@gmail.com>
2022-03-09 23:56:05 -0500

    Added env files

--- greenCan/reuse/urls.py
+++ greenCan/reuse/urls.py
@@ -7,6 +7,6 @@ app_name = "reuse"
 urlpatterns = [
     path("", views.index, name="reuse-landing-page"),
     path("donations", views.donation_view, name="donation-page"),
-    path("listings", views.listingPage, name="listingPage"),
-    path("create_post", views.create_post, name="create-post"),
+    path('listings', views.listingPage, name='listingPage'),
+    path('create_post', views.create_post, name='create-post')
 ]
