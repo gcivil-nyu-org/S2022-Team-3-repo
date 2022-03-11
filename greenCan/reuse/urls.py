058327bf3f43bdab1c4f5d3b4f81b7aacdd59e87
Kevin Patel <patelkvin04@gmail.com>
2022-03-11 01:10:51 -0500

    ui bugfix

--- greenCan/reuse/urls.py
+++ greenCan/reuse/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "reuse"
 
 urlpatterns = [
-    path("", views.index, name="reuse-landing-page"),
+    path("", views.index, name="index"),
     path("donations", views.donation_view, name="donation-page"),
     path("listings", views.listingPage, name="listingPage"),
     path("create_post", views.create_post, name="create-post"),
