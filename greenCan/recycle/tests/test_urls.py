737f723636faf71acbf7cafe2b17328ffa0a1003
Suyash Soniminde <67739920+TheHarbinger99@users.noreply.github.com>
2022-03-23 11:26:44 +0530

    Add files via upload

---
+++ greenCan/recycle/tests/test_urls.py
@@ -0,0 +1,21 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve 
+from recycle.views import (
+    index,
+    get_dropoff_locations,
+    search_dropoff_locations
+)
+
+class TestUrls(SimpleTestCase):
+
+    def test_index_url_is_resolved(self):
+        url = reverse('recycle:index')
+        self.assertEquals(resolve(url).func, index)
+        self.assertEquals(resolve(url).namespace, 'recycle')
+        self.assertEquals(url, '/recycle/')
+
+    def test_fetchdropfflocations_url_is_resolved(self):
+        url = reverse('recycle:fetch-drop-off-locations')
+        self.assertEquals(resolve(url).func, search_dropoff_locations)
+        self.assertEquals(resolve(url).namespace, 'recycle')
+        self.assertEquals(url, '/recycle/fetch-drop-off-locations')
