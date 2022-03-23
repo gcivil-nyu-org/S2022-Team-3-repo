7d0bb68ec5d1f168532f5d052a5817d743c64516
bojianwangny <bw2197@nyu.edu>
2022-03-22 23:34:40 -0400

    add tests

--- greenCan/greenCan/settings.py
+++ greenCan/greenCan/settings.py
@@ -198,7 +198,7 @@ ACCOUNT_AUTHENTICATION_METHOD = "email"
 ACCOUNT_AUTHENTICATED_LOGIN_REDIRECTS = False
 SOCIALACCOUNT_LOGIN_ON_GET = True  # bypass the "do you want to login page"
 SOCIALACCOUNT_PROVIDERS = {
-    "google": {"SCOPE": ["profile", "email"], "AUTH_PARAMS": {"access_type": "online"},}
+    "google": {"SCOPE": ["profile", "email"], "AUTH_PARAMS": {"access_type": "online"}}
 }
 
 AUTHENTICATION_BACKENDS = [
