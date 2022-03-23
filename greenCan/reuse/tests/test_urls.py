9e6145456e0b68a4e140674a3126737d73db1ae7
bojianwangny <bw2197@nyu.edu>
2022-03-23 01:02:35 -0400

    adding comments

--- greenCan/reuse/tests/test_urls.py
+++ greenCan/reuse/tests/test_urls.py
@@ -1,3 +1,4 @@
+"""
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from reuse.views import index, donation_view, listing_page, create_post
@@ -8,14 +9,18 @@ class TestUrls(SimpleTestCase):
         url = reverse("index")
         self.assertEqual(resolve(url).func, index)
 
+
     def test_donation_view(self):
         url = reverse("donation-page")
         self.assertEqual(resolve(url).func, donation_view)
+
 
     def test_listing_page(self):
         url = reverse("listingPage")
         self.assertEqual(resolve(url).func, listing_page)
 
+
     def test_create_post(self):
         url = reverse("create-post")
         self.assertEqual(resolve(url).func, create_post)
+"""
