3743f423f20a8b67ebc386818c13a9a14703a8e6
bojianwangny <bw2197@nyu.edu>
2022-03-30 18:15:08 -0400

    adding tests

--- greenCan/reuse/tests/test_models.py
+++ greenCan/reuse/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from model_bakery import baker
-from reuse.models import Post, Image
+from reuse.models import Post, Image, NGOLocation
 
 
 class TestModels(TestCase):
@@ -11,3 +11,7 @@ class TestModels(TestCase):
     def test_Image_model(self):
         image = baker.make("reuse.Image")
         self.assertIsInstance(image, Image)
+
+    def test_NGOLocation_model(self):
+        ngo_location = baker.make("reuse.NGOLocation")
+        self.assertIsInstance(ngo_location, NGOLocation)
