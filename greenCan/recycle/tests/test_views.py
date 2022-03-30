9d184c9f1af09a03dca7730378a7ccc714e8fe69
meisun0107 <0107sun.mei@gmail.com>
2022-03-30 00:38:03 -0400

    Merge pull request #141 from gcivil-nyu-org/mei_profilePage
    Fix some UI bugs

--- greenCan/recycle/tests/test_views.py
+++ greenCan/recycle/tests/test_views.py
@@ -48,16 +48,16 @@ class TestViews(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, "recycle/templates/index.html")
 
-    # def test_searchdropofflocations1(self):
+    def test_searchdropofflocations1(self):
 
-    #     print(self.zipcode.id)
+        print(self.zipcode.id)
 
-    #     response = self.client.get(self.url + "?type=live-location&latitude=40.7362&longitude=-74.0422")
+        response = self.client.get(self.url + "?type=live-location&latitude=40.7362&longitude=-74.0422")
 
-    #     self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.status_code, 200)
 
-    # def test_searchdropofflocations2(self):
+    def test_searchdropofflocations2(self):
 
-    #     response = self.client.get(self.url + "?type=zipcode&zipcode=10004")
+        response = self.client.get(self.url + "?type=zipcode&zipcode=10004")
 
-    #     self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.status_code, 200)
