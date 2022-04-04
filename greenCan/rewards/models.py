5d211016ffc42d315b8846d8d7ea8648eb44d606
Kevin Patel <patelkvin04@gmail.com>
2022-04-04 12:50:37 -0400

    Resolved the bug

--- greenCan/rewards/models.py
+++ greenCan/rewards/models.py
@@ -13,14 +13,19 @@ class Event(models.Model):
     name = models.CharField(max_length=100, null=False, unique=True)
 
 
-class Image(models.Model):
+class ImageMeta(models.Model):
     id = models.BigAutoField(auto_created=True, primary_key=True)
     category = models.ManyToManyField(Category)
     event_type = models.ForeignKey(Event, null=False, on_delete=models.CASCADE)
-    image = models.URLField(max_length=2000, null=False, unique=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, null=False, on_delete=models.CASCADE)
     location = models.ForeignKey(ZipCode, null=True, on_delete=models.CASCADE)
     caption = models.CharField(max_length=150, null=True)
     consent = models.BooleanField(default=False)
+
+
+class Image(models.Model):
+    id = models.BigAutoField(auto_created=True, primary_key=True)
+    meta = models.ForeignKey(ImageMeta, on_delete=models.CASCADE)
+    image = models.URLField(max_length=2000, null=False, unique=True)
     approved = models.BooleanField(default=False)
     uploaded_on = models.DateTimeField(auto_now_add=True)
