6ee7ae8cd26e773cea816a1761eafd10e1266f53
pb2640 <pb2640@nyu.edu>
2022-04-16 14:48:42 -0400

    volunteer admin

--- greenCan/reuse/admin.py
+++ greenCan/reuse/admin.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
 
 # from .models import Post, Image, NGOLocation
-from . import models
+from .models import Post,Image,NGOLocation
 
 class VolunteerAdmin(admin.AdminSite):
     site_header = "Volunteer Moderation page/"
 
+class PostAdmin(admin.ModelAdmin):
+    fields = ["title","category","approved"]
+    def has_add_permission(self,request):
+        return False    
+    def has_delete_permission(self,request,obj = None):
+        return False
+
+
 volunteer_site = VolunteerAdmin(name="VolunteerAdmin")
-volunteer_site.register(models.Post)
-volunteer_site.register(models.Image)
-volunteer_site.register(models.NGOLocation)
+volunteer_site.register(Post,PostAdmin)
+volunteer_site.register(Image)
+volunteer_site.register(NGOLocation)
 # admin.site.register(Post)
 # admin.site.register(Image)
 # admin.site.register(NGOLocation)
