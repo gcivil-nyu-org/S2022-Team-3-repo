544e805483451ec88f78b07c083688ecc25fb99b
Kevin Patel <patelkvin04@gmail.com>
2022-04-19 15:46:46 -0400

    Update settings.py

--- greenCan/greenCan/settings.py
+++ greenCan/greenCan/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
-)
+).read_env()
 
 # environ.Env.read_env(BASE_DIR / ".env-dev")
-environ.Env.read_env()
+# environ.Env
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
