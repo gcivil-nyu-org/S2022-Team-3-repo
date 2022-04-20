27b5ea965b5e7126115d51e8fd22cd71c48d0958
bojianwangny <bw2197@nyu.edu>
2022-04-20 01:26:06 -0400

    add utils.py

--- greenCan/notification/views.py
+++ greenCan/notification/views.py
@@ -1,18 +1,1 @@
-from django.shortcuts import render
-from notifications.signals import notify
 
-
-def create_notificaiton(notification):
-    sender = notification.get("sender")
-    receiver = notification.get("receiver")
-    message = notification.get("message")
-    msg_type = notification.get("msg_type")
-
-    notification = Notification(
-        user=receiver,
-        message=message,
-        message_type=msg_type,
-    )
-    notification.save()
-
-    notify.send(sender, recipient=receiver, verb='Notification', description=message)
