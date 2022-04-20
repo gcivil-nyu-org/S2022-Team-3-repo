ba5b00f49e9420ed77f3b0a1019d2c6ec20917ea
bojianwangny <bw2197@nyu.edu>
2022-04-20 02:23:36 -0400

    add index page

--- greenCan/notification/views.py
+++ greenCan/notification/views.py
@@ -1,1 +1,14 @@
+from django.shortcuts import render
 
+
+"""
+function: index
+
+set path for notification page
+"""
+
+
+def index(request):
+    template = "notification/templates/notification-index.html"
+    context = {"is_notification": True}
+    return render(request, template, context=context)
