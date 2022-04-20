b92833b130b157438f5dddf8e7a386981a65c3f2
bojianwangny <bw2197@nyu.edu>
2022-04-20 02:35:27 -0400

    add index page

--- greenCan/notification/views.py
+++ greenCan/notification/views.py
@@ -12,3 +12,13 @@ def index(request):
     template = "notification/templates/notification-index.html"
     context = {"is_notification": True}
     return render(request, template, context=context)
+
+
+"""
+function: get_notifications
+
+get all notifications for a specific user
+"""
+
+def get_notifications(request):
+    
