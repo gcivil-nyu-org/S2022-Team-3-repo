b7b97f00f0cf7904aeef5842c65ca31a055d8eaf
Kevin Patel <patelkvin04@gmail.com>
2022-04-27 13:19:05 -0400

    Update test_models.py

---
+++ greenCan/moderation/tests/test_models.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from moderation.models import VolunteerLogs
+from model_bakery import baker
+
+
+class TestModerationModels(TestCase):
+
+    def test_volunteer_logs_model(self):
+        volunteer_log = baker.make("moderation.VolunteerLogs")
+        self.assertIsInstance(volunteer_log, VolunteerLogs)
