0a17d2a7f67c47d1d0604d1098f680707c638578
pb2640 <pb2640@nyu.edu>
2022-04-27 10:02:50 -0400

    Updated vlog model

--- greenCan/moderation/models.py
+++ greenCan/moderation/models.py
@@ -9,12 +9,10 @@ from django.contrib.contenttypes.models import ContentType
 class VolunteerLogs(models.Model):
     id = models.AutoField(primary_key=True, auto_created=True)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    # add a content_type here
     content_object = GenericForeignKey("content_type", "id")
     reason = models.CharField(max_length=250, null=False, default="None")
     approved = models.CharField(default="False", max_length=10)
     approved_by = models.CharField(max_length=100, null=False, default="nyc.greencan@gmail.com")
-    # user_email = models.EmailField(null=True) #email of users post
 
     @property
     def is_approved(self):
@@ -23,13 +21,6 @@ class VolunteerLogs(models.Model):
             return str(self.approved)
         else:
             return str(self.approved)
-
-    # @property
-    # def user_email(self):
-    #     if(self.content_object==Post):
-    #         return self.content_object.email
-    #     else:
-    #         return str(self.content_object.user.email)
 
     def __str__(self):
         return str(self.id)
