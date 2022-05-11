8dad89b2800984b454ebfe50d3a896f95aa644e7
Kevin Patel <patelkvin04@gmail.com>
2022-05-10 23:23:09 -0400

    Solved black errors

--- greenCan/reuse/tests/test_admin.py
+++ greenCan/reuse/tests/test_admin.py
@@ -11,11 +11,8 @@ from django.contrib.admin.sites import AdminSite
 
 def get_admin_change_view_url(obj: object) -> str:
     return reverse(
-        'admin:{}_{}_change'.format(
-            obj._meta.app_label,
-            type(obj).__name__.lower()
-        ),
-        args=(obj.pk,)
+        "admin:{}_{}_change".format(obj._meta.app_label, type(obj).__name__.lower()),
+        args=(obj.pk,),
     )
 
 
