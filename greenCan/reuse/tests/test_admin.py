b56d4326eda191a900069c75d02cf4dd46e58de8
bojianwangny <bw2197@nyu.edu>
2022-05-11 00:34:29 -0400

    changes

--- greenCan/reuse/tests/test_admin.py
+++ greenCan/reuse/tests/test_admin.py
@@ -1,10 +1,6 @@
 from django.test import TestCase
-from reuse.models import Post, PostConcernLogs
-from recycle.models import ZipCode
-from django.contrib.auth import get_user_model
-from rewards.models import CreditsLookUp
-from reuse.admin import PostConcernLogsAdmin, PostAdmin
-from django.contrib.admin.sites import site
+from reuse.models import Post
+from reuse.admin import PostAdmin
 # from django.urls import reverse
 from django.contrib.admin.sites import AdminSite
 # from django.conf import settings
@@ -19,64 +15,63 @@ from django.contrib.admin.sites import AdminSite
 #         args=(obj.pk,)
 #     )
 
+# class TestPostConcernLogsAdmin(TestCase):
+#     def setUp(self):
+#         User = get_user_model()
+#         user = User.objects.create(
+#             email="user@gmail.com",
+#             password="password1",
+#             first_name="john",
+#             last_name="doe",
+#         )
+#         self.user = user
 
-class TestPostConcernLogsAdmin(TestCase):
-    def setUp(self):
-        User = get_user_model()
-        user = User.objects.create(
-            email="user@gmail.com",
-            password="password1",
-            first_name="john",
-            last_name="doe",
-        )
-        self.user = user
+#         self.admin = User.objects.create(
+#             email="testemail@gmail.com",
+#             password="password1",
+#             first_name="john",
+#             last_name="doe",
+#             admin=True,
+#         )
 
-        self.admin = User.objects.create(
-            email="testemail@gmail.com",
-            password="password1",
-            first_name="john",
-            last_name="doe",
-            admin=True,
-        )
+#         zipcode = ZipCode(
+#             zip_code="10001",
+#             state_id="NY",
+#             state="New York",
+#             borough="Manhattan",
+#             centroid_latitude=40.75021293296376,
+#             centroid_longitude=-73.99692994900218,
+#             polygon="",
+#         )
+#         zipcode.save()
+#         self.zipcode = zipcode
 
-        zipcode = ZipCode(
-            zip_code="10001",
-            state_id="NY",
-            state="New York",
-            borough="Manhattan",
-            centroid_latitude=40.75021293296376,
-            centroid_longitude=-73.99692994900218,
-            polygon="",
-        )
-        zipcode.save()
-        self.zipcode = zipcode
+#         post = Post(
+#             title="Apple",
+#             category="Books",
+#             phone_number="9175185345",
+#             email="user1@gmail.com",
+#             zip_code=self.zipcode,
+#             description=" Book on apple",
+#             user=self.user,
+#             approved=False,
+#         )
+#         post.save()
+#         self.post = post
 
-        post = Post(
-            title="Apple",
-            category="Books",
-            phone_number="9175185345",
-            email="user1@gmail.com",
-            zip_code=self.zipcode,
-            description=" Book on apple",
-            user=self.user,
-            approved=False,
-        )
-        post.save()
-        self.post = post
+#         post_concern = PostConcernLogs(post=self.post, checked=False, message="Approved!")
+#         post_concern.save()
+#         self.post_concern = post_concern
+#         CreditsLookUp.objects.create(action="post", credit=10)
 
-        post_concern = PostConcernLogs(post=self.post, checked=False, message="Approved!")
-        post_concern.save()
-        self.post_concern = post_concern
-        CreditsLookUp.objects.create(action="post", credit=10)
+#         self.post_concern_log_admin = PostConcernLogsAdmin(PostConcernLogs, site)
 
-        self.post_concern_log_admin = PostConcernLogsAdmin(PostConcernLogs, site)
-
-    # def test_change_view(self):
-    #     self.client.force_login(self.admin, backend=settings.AUTHENTICATION_BACKENDS[0])
-    #     url = get_admin_change_view_url(self.post_concern)
-    #     response = self.client.get(url, follow=True)
-    #     self.assertIn(response.status_code, [200])
-    #     self.assertIs(response.resolver_match.func.__name__, "change_view")
+#     def test_change_view(self):
+#         self.client.force_login(self.admin, backend=settings.AUTHENTICATION_BACKENDS[0])
+#         url = get_admin_change_view_url(self.post_concern)
+#         response = self.client.get(url, follow=True)
+#         self.assertIn(response.status_code, [200])
+#         self.assertIs(response.resolver_match.func.__name__, "change_view")
 
 
 class OurRequest(object):
